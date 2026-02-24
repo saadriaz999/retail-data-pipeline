f53fda9e8988ba271b7f73c1bd520a1370dc45c9
Saad Riazuddin Mahmood <saadriaz999@gmail.com>
2026-02-23 20:34:55 -0600

    update readem

--- data-fetcher/app.py
+++ data-fetcher/app.py
@@ -33,4 +33,4 @@ def dimension_tables():
     return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
---
+++ data-pipeline/lambda/dimension_loader.py
@@ -0,0 +1,43 @@
+import requests
+import pandas as pd
+import boto3
+import os
+from io import BytesIO
+
+s3 = boto3.client("s3")
+
+RAW_BUCKET = os.environ["RAW_BUCKET"]
+PROCESSED_BUCKET = os.environ["PROCESSED_BUCKET"]
+API_URL = os.environ["API_URL"]
+
+
+def lambda_handler(event, context):
+    print("Fetching dimension tables")
+
+    response = requests.get(f"{API_URL}/dimension_tables")
+    data = response.json()
+
+    for table, rows in data["data"].items():
+        df = pd.DataFrame(rows)
+
+        # ---------- RAW JSON ----------
+        json_key = f"dimensions/{table}.json"
+        s3.put_object(
+            Bucket=RAW_BUCKET,
+            Key=json_key,
+            Body=df.to_json(orient="records")
+        )
+
+        # ---------- PROCESSED PARQUET ----------
+        buffer = BytesIO()
+        df.to_parquet(buffer, index=False)
+
+        parquet_key = f"dimensions/{table}/{table}.parquet"
+
+        s3.put_object(
+            Bucket=PROCESSED_BUCKET,
+            Key=parquet_key,
+            Body=buffer.getvalue()
+        )
+
+    return {"status": "success"}
